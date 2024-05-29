70fa1ee3cdd5058eab66aff286fd4f27cbe65b50
Shravan kumar <eldanditechnologies@gmail.com>
2024-05-29 06:53:44 +0530

    Updated

---
+++ Planning & Analysis/Project Planning.php
@@ -0,0 +1,5 @@
+Studying the Requirements and Planning:
+
+Estimated Hours: 8
+
+Cost: $20/hour = $160
---
+++ UIUX Design/User interface design.php
@@ -0,0 +1,5 @@
+The approved Frontend and Backend UI Design will be implemented on the platform
+
+Estimated Hours: 40
+
+Cost: $20/hour = $800
