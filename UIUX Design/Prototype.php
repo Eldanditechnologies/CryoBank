149eca174990a38ad4386776bc8d92f86f295a6e
Eldanditechnologies <eldanditechnologies@gmail.com>
2024-05-29 14:04:12 +0530

    Update Prototype.php

--- UIUX Design/Prototype.php
+++ UIUX Design/Prototype.php
@@ -1,5 +1,5 @@
 Designing the frontend and bacnkend UI Prototypes and sharing it with client either on Figma or anyother medium for approval
 
-Estimated Hours: 60
+Estimated Hours: 
 
-Cost: $20/hour = $1200
+Cost: $20/hour =
