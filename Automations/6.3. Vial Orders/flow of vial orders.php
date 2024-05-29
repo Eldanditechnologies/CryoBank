1e71d9ce456527261bd45350d5c78d77a0f3f32b
Eldanditechnologies <eldanditechnologies@gmail.com>
2024-05-29 06:56:59 +0530

    Delete Automations/6.3. Vial Orders directory

--- Automations/6.3. Vial Orders/flow of vial orders.php
+++
@@ -1,2 +0,0 @@
-Frontend: Submit —> Payment Gateway (stripe) —> Email Notification [forms signed/forms incomplete] —> Link to Calendar   $50
-Backend: Payment Gateway —> Accept —> Admin Dashboard —>Deliveries —> Calendar —> Scheduling —> Processing —> New Orders $50
