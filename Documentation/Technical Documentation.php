d90c9b6cf6c9c0b2ed7d7c5de526bfe342dd2ad9
Shravan kumar <eldanditechnologies@gmail.com>
2024-05-29 13:49:49 +0530

    update

---
+++ Documentation/Technical Documentation.php
@@ -0,0 +1,5 @@
+Technical documentation
+
+Estimated Hours: 30 hours
+
+Cost: $20/hour = $600
---
+++ Testing and QA/QA Testing.php
@@ -0,0 +1,7 @@
+Unit testing
+Integration testing
+User acceptance testing
+
+Estimated Hours: 100 hours
+
+Cost: $20/hour = $2000
